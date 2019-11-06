c69b2fef9268a9f24d68e0db6f800dbef4f67d20
Sharafat Ali <codemented@gmail.com>
2019-11-06 08:40:58 +0500

    added header

--- include/php/basic/if-else.php
+++ include/php/basic/if-else.php
@@ -4,7 +4,6 @@ echo "<div class='container pt-5'>";
 define("Ref", "#");
 define("Vid", "https://www.youtube.com/watch?v=T6ULdzlYWXk&list=PLbGui_ZYuhigFdLdbSI2EM2MrJB7I0j-B&index=28");
 
-
 $s = 10;
 
 if ($s == 10 && $s < 11):
@@ -12,10 +11,6 @@ if ($s == 10 && $s < 11):
 else:
     echo "Condition False";
 endif;
-
-
-
-
 
 
 ?><br><br>
