8eb995e075e526ed64fac59b99fb440d626f8e1b
CoDemented <52868823+CoDemented@users.noreply.github.com>
2020-02-04 23:50:56 +0500

    added just field

--- workshop/base_pdo_crud/index.php
+++ workshop/base_pdo_crud/index.php
@@ -2,16 +2,33 @@
     include_once 'connection.php';
 
 //----------------------------------------------- INSERT
-//insert('This is my first record');
+if (isset($_POST['sub'])) {
+    $un = $_POST['un'];
+    if ($un == NULL){
+        $res = 'Enter name';
+    }
+    else {
+        insert($un);
+        $res = 'Added';
+    }
+}
 
 
 //---------------------------------------------------------- UPDATE
-//update(2, 'Demo2');
+//update(3, 'Demo3');
 
 
 //---------------------------------------------------------------------- DELETE
-//delete(10);
+//delete(5);
 
 
 //--------------------------------- SELECT / PRINT
 print_row();
+?>
+<form action="" method="post">
+    <input type="text" name="un">
+    <input type="submit" name="sub" value="Add +">
+</form>
+<?php
+    if (isset($res)){echo $res;}
+?>
